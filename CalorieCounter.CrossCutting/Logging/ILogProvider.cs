015e11df673f6cc3a158979451332eaf0ea5aacc
Harry <carlosgarciaharry@hotmail.com>
2020-08-06 14:21:39 -0600

    Logs bug fixed.

---
+++ CalorieCounter.CrossCutting/Logging/ILogProvider.cs
@@ -0,0 +1,11 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace CalorieCounter.CrossCutting.Logs
+{
+    public interface ILogProvider
+    {
+        void SaveInfo(IInvocation invocation);
+        void SaveError(IInvocation invocation, Exception exception);
+    }
+}
---
+++ CalorieCounter.CrossCutting/Logging/LogInterceptor.cs
@@ -0,0 +1,31 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace CalorieCounter.CrossCutting.Logs
+{
+    public class LogInterceptor : IInterceptor
+    {
+        private readonly ILogProvider _logProvider;
+
+        public LogInterceptor(ILogProvider logProvider)
+        {
+            _logProvider = logProvider;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            try
+            {
+                _logProvider.SaveInfo(invocation);
+                invocation.Proceed();
+                _logProvider.SaveInfo(invocation);
+            }
+            catch (Exception e)
+            {
+                _logProvider.SaveError(invocation, e);
+                throw e;
+            }
+
+        }
+    }
+}
